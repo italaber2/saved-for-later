3b999ee793652cf379ee8f19573e02fe1d2072f7
Ian Talaber <ian.talaber@accenture.com>
2023-05-17 16:43:24 +0200

    extension added

--- src/index.tsx
+++ src/index.tsx
@@ -3,11 +3,21 @@ import ReactDOM from 'react-dom/client';
 import './style/app.css';
 import App from './components/App';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(
+const root = document.createElement("div")
+root.className = "container"
+document.body.appendChild(root)
+const rootDiv = ReactDOM.createRoot(root);
+rootDiv.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
+
+// const root = ReactDOM.createRoot(
+//   document.getElementById('root') as HTMLElement
+// );
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
