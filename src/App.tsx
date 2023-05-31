16475983601dd3d14e7a21af5b313c26ac59ecbc
Ian Talaber <ian.talaber@accenture.com>
2023-05-31 17:07:54 +0200

    bookmarks working in extension

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,39 @@
 //import React from 'react';
 import { useEffect, useState } from 'react';
-import { bookmarkLinks } from "./data";
+
+interface BookmarkObject {
+  title: string,
+  url: string,
+  dateAdded: number
+ }
+
+//general refactoring
+//figure out why the new bookmark isn't working
+//refine bookmark search by oldest + only oldest 6 elements
+//clickable links
+
+function App() {
+const [bookmarkLinks, setBookmarkLinks] = useState ([] as any)
+  useEffect(() => {
+    chrome.bookmarks.getTree((bookmarkTreeNodes: any) => {
+      setBookmarkLinks(extractBookmark(bookmarkTreeNodes));
+    });
+  }, []);
+
+  const extractBookmark: any = (nodes: any) => {
+    let bookmarks: any = [];
+
+    for (let i = 0; i < nodes.length; i++) {
+      const node = nodes[i];
+      if (node.children) {
+        bookmarks = bookmarks.concat(extractBookmark(node.children));
+      } else {
+        bookmarks.push({ title: node.title, url: node.url, dateAdded: node.dateAdded });
+      }
+    }
+
+    return bookmarks;
+  };
 
 function BookmarkText (title: string) {
   return (
@@ -18,58 +51,9 @@ function BookmarkImg (img: string) {
   )
 }
 
-function DumpBookmarks() {
-  useEffect(() => {
-    chrome.bookmarks.getTree((bookmarkTreeNodes: any) => {
-      const bookmarks = extractBookmark(bookmarkTreeNodes);
-      downloadBookmarks(bookmarks);
-    });
-  }, []);
-
-  const extractBookmark: any = (nodes: any) => {
-    let bookmarks: any = [];
-
-    for (let i = 0; i < nodes.length; i++) {
-      const node = nodes[i];
-      if (node.children) {
-        bookmarks = bookmarks.concat(extractBookmark(node.children));
-      } else {
-        bookmarks.push({ title: node.title, url: node.url });
-      }
-    }
-
-    return bookmarks;
-  };
-
-  const downloadBookmarks = (bookmarks: any) => {
-    const content = JSON.stringify(bookmarks, null, 2);
-    const blob = new Blob([content], { type: 'application/json' });
-    const url = URL.createObjectURL(blob);
-
-    const a = document.createElement('a');
-    a.href = url;
-    a.download = 'bookmarks.json';
-    a.click();
-
-    URL.revokeObjectURL(url);
-  };
-
-  return (
-    <div className="App">
-      <h1>Bookmark Downloader</h1>
-      <p>Click the button to download your bookmarks.</p>
-      <button onClick={() => window.close()}>Close</button>
-    </div>
-  );
-}
-
-//figure out how to import bookmark data (Chrome documentation)
-//need import bookmark function
-//need sort bookmark data by six oldest function
-
-async function getBookmarkData (bookmarkUrl: string) {
+async function getBookmarkData (bookmarkObject: BookmarkObject) {
   const response = await fetch(
-    "https://jsonlink.io/api/extract?url=" + bookmarkUrl,
+    "https://jsonlink.io/api/extract?url=" + bookmarkObject.url,
     {
       method: "GET",
     }
@@ -78,11 +62,11 @@ async function getBookmarkData (bookmarkUrl: string) {
   return myJson;
 };
 
-function SecondaryBookmark (bookmarkUrl: string) {
+function SecondaryBookmark (bookmarkObject: BookmarkObject) {
   const [bookmarkData, setBookmarkData] = useState ({} as any)
   useEffect(()=> {
     async function retrieveBookmarkData() {
-      const returnValue = await getBookmarkData(bookmarkUrl);
+      const returnValue = await getBookmarkData(bookmarkObject);
       setBookmarkData(returnValue);
     }
       retrieveBookmarkData();
@@ -90,36 +74,18 @@ function SecondaryBookmark (bookmarkUrl: string) {
   );
 
   return (
-    <div className='secondaryBookmark' key={bookmarkUrl}>
+    <div className='secondaryBookmark' key={bookmarkObject.url}>
       {BookmarkText(bookmarkData.title as string)}
       {BookmarkImg(bookmarkData.images as string)}
     </div>
   )
 }
 
-// function PrimaryBookmark () {
-//   const [bookmarkData, setBookmarkData] = useState ({} as any)
-//   useEffect(()=> {
-//     async function retrieveBookmarkData() {
-//       const firstBookmark = bookmarkLinks[0]
-//       const returnValue = await getBookmarkData(firstBookmark);
-//       setBookmarkData(returnValue);
-//     }
-//     retrieveBookmarkData();
-//   }, []
-//   );
-
-//   return (
-//     <>{BookmarkText(bookmarkData.title as string, bookmarkData.description as string)}
-//     {BookmarkImg(bookmarkData.images as string)}</>
-//   )
-// }
-
 function SecondaryBookmarksViewport () {
-  const slicedBookmarkLinks = bookmarkLinks.slice(1);
+  const slicedBookmarkLinks = bookmarkLinks;
   console.log(slicedBookmarkLinks);
   // Need to fix multiple calls as documented above
-  const bookmarkComponents = slicedBookmarkLinks.map((bookmarkLink)=> {
+  const bookmarkComponents = slicedBookmarkLinks.map((bookmarkLink:any)=> {
       return SecondaryBookmark(bookmarkLink)
      }
     )
@@ -130,38 +96,19 @@ function SecondaryBookmarksViewport () {
   )
 }
 
-// function PrimaryBookmarkViewport () {
-//   return (
-//     <div className='primaryBookmarkViewport'>
-//       <PrimaryBookmark/>
-//     </div>
-//   )
-// }
-
 function BookmarkViewports () {
   return (
     <div className='bookmarksViewport'>
-      {/* <PrimaryBookmarkViewport/> */}
       <SecondaryBookmarksViewport/>
     </div>
   )
 }
 
-// function NavBar () {
-//   return (
-//     <div className='navBar'>
-//       Navigate me somewhere interesting
-//     </div>
-//   )
-// }
-
-function App() {
   return (
     <div className='mainViewport'>
-      {/* <NavBar/> */}
       <BookmarkViewports/>
       </div>
   );
 }
 
-export default App;
+ export default App;
